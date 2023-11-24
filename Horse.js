f8da29244de69b538f4c7dfc300474c3f65a0c61
tuandk123 <01632399987tuan@gmail.com>
2023-11-24 11:54:55 +0700

    Horse run method

--- Horse.js
+++ Horse.js
@@ -1,8 +1,13 @@
+
 function Horse() {
 }
 
 Horse.prototype.Jump = function(){
     console.log('Jumped.....');
+};
+
+Horse.prototype.run = function(){
+    console.log('Running');
 };
 
 module.exports = Horse;
